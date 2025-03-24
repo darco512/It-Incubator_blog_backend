c7518f180425519df780cd2935c4dc1a22e5a087
darco512 <darcohaha@gmail.com>
2025-03-25 01:05:58 +0200

    found typo in paginatiionValues for searchNameTerms

--- src/helpers/paginations-values.ts
+++ src/helpers/paginations-values.ts
@@ -6,8 +6,10 @@ export const paginationQueries = (req: Request) => {
     let pageSize: number = req.query.pageSize ? +req.query.pageSize : 10;
     let sortBy: string = req.query.sortBy ? req.query.sortBy.toString() : 'createdAt';
     let sortDirection :SortDirection = req.query.sortDirection && req.query.sortDirection === 'asc' ? 'asc' : 'desc';
-    let searchNameTerm: string | null = req.query.seacrhNameTerm ? req.query.seacrhNameTerm.toString() : null;
+    let searchNameTerm: string | null = req.query.searchNameTerm ? req.query.searchNameTerm.toString() : null;
     let blogId: string | null = req.params.id ? req.params.id : null;
+
+
 
     return {pageNumber, pageSize, sortBy, sortDirection, searchNameTerm, blogId}
 }
