4239757bc0eb3f7999dd8eea6eec12276b4c3ed5
darco512 <darcohaha@gmail.com>
2026-01-15 01:00:44 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,22 @@ import {app} from './app'
 import {SETTINGS} from './settings'
 import {runDB} from './db/mongo-db'
 
+// For Vercel serverless, export the app
+export default app
 
-const startApp = async () => {
-    const res = await runDB(SETTINGS.MONGO_URL);
-    if(!res)process.exit(1)
+// For local development, start the server (not in Vercel environment)
+if (!process.env.VERCEL) {
+    const startApp = async () => {
+        const res = await runDB(SETTINGS.MONGO_URL);
+        if (!res) {
+            console.error('Failed to connect to database')
+            process.exit(1)
+        }
 
-    app.listen(SETTINGS.PORT, () => {
-        console.log('...server started in port ' + SETTINGS.PORT)
-    })
+        app.listen(SETTINGS.PORT, () => {
+            console.log('...server started in port ' + SETTINGS.PORT)
+        })
+    }
+    startApp()
 }
-
-startApp()
 
