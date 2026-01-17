d599b3ed5630a483284f4c208360bbd1728aeb52
darco <darcohaha@gmail.com>
2026-01-17 02:51:23 +0200

    123

--- src/routers/auth-router.ts
+++ src/routers/auth-router.ts
@@ -44,8 +44,6 @@ authRouter.get('/me',
 
 
 authRouter.post('/registration',
-    userInputsValidation,
-    inputValidationMiddleware,
     async (req: Request, res: Response) => {
         // Check which field is duplicated before creating user
         const existingUserByLogin = await usersQueriesRepository.findUserByLogin(req.body.login);
